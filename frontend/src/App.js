314cf0981acadecbd86d08ce35e1b75d5ba8bece
SuayMack <pry_chu@hotmail.com>
2020-12-26 02:17:57 -0300

    update folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,8 @@ function App() {
                     <a className="brand" href="index.html">amazona</a>
                 </div>
                 <div>
-                    <a href="cart.html">Cart</a>
-                    <a href="singin.html">Sing In</a>
+                    <a href="/cart">Cart</a>
+                    <a href="/singin">Sing In</a>
                 </div>
             </header>
             <main>
@@ -19,10 +19,14 @@ function App() {
                         data.products.map((product) => (
                             <div key={product._id} className="card">
                                 <a href={`/product/${product._id}`}>
-                                    <img className="medium" src="./images/p1.jpg" alt="product"/>
+                                    <img 
+                                        className="medium" 
+                                        src={product.image} 
+                                        alt={product.name}
+                                    />
                                 </a>
                                 <div className="card-body">
-                                    <a href="product.html">
+                                <a href={`/product/${product._id}`}>
                                         <h2>{product.name}</h2>
                                     </a>
                                     <div className="rating">
@@ -32,7 +36,7 @@ function App() {
                                         <span><i className="fa fa-star"></i></span>
                                         <span><i className="fa fa-star"></i></span>
                                     </div>
-                                    <div className="price">$120</div>
+                                    <div className="price">${product.price}</div>
                                 </div>             
                             </div>
                         ))
