710a974440ccb811937e58800d76f78dff461fae
SuayMack <pry_chu@hotmail.com>
2021-01-01 13:31:59 -0300

    Create Node.JS Server

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-const port = process.env.PORT || 500;
+const port = process.env.PORT || 5000;
 app.listen(port,  () => {
     console.log(`Server at http://localhost:${port}`);
 });
