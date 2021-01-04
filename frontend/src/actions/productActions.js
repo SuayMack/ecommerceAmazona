e6a81e35862c25b884fa8580a747d0eb1091509f
SuayMack <pry_chu@hotmail.com>
2021-01-04 01:42:45 -0300

    Add Redux to Product Screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,9 +1,14 @@
 import Axios from 'axios';
-import {
-  PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_REQUEST,
+import {  
   PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_FAIL,
+
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,  
 } from '../constants/productConstants';
+
 
 export const listProducts = () => async (dispatch) => {
   dispatch({
@@ -16,3 +21,16 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
   }
 };
+
+export const detailsProduct = (productId) => async(dispatch) =>{
+  dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productId});
+  try{
+    const { data } = Axios.get(`/api/products/${productId}`);
+    dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data})
+  }catch(error){
+    dispatch({type: PRODUCT_DETAILS_FAIL, payload: error.response.data.message
+      ? error.response.data.message
+      : error.message,
+    });
+  }
+}
