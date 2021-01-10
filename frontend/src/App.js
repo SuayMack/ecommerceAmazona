5b8aabe5368c96228b23f19e29ce9d9eea7c6392
SuayMack <pry_chu@hotmail.com>
2021-01-10 19:54:24 -0300

    Render cartItems.length

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,13 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function App() {
     
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart; 
+
     return (
         <BrowserRouter>
             <div className="grid-container">
@@ -14,6 +18,9 @@ function App() {
                     </div>
                     <div>
                         <Link to="/cart">Cart</Link>
+                        {cartItems.length > 0 && (
+                            <span className="badge">{cartItems.length}</span>
+                        )}
                         <Link to="/singin">Sing In</Link>
                     </div>
                 </header>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,17 +3,23 @@ import thunk from 'redux-thunk';
 import { cartReducers } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
-const initialState = {};
+const initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems') ?
+            JSON.parse(localStorage.getItem('cartItems')) :
+            [],
+    },
+};
 const reducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-  cart: cartReducers, 
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducers,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
