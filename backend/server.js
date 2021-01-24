3900f47210e678ff4154670b6ba712f7e058fb8c
SuayMack <pry_chu@hotmail.com>
2021-01-24 12:25:09 -0300

    Upload Product Image

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config();
 
@@ -17,12 +19,15 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useCreateIndex: true,
 });
 
+app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
@@ -33,5 +38,5 @@ app.use((err, req, res, next) =>{
 
 const port = process.env.PORT || 5000;
 app.listen(port,  () => {
-    console.log(`Server at http://localhost:${port}`);
+    console.log(`Serve at http://localhost:${port}`);
 });
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -4,6 +4,7 @@ import { detailsProduct, updateProduct } from '../actions/productActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
+import Axios from 'axios';
 
 export default function ProductEditScreen(props) {
    const productId = props.match.params.id;
@@ -48,6 +49,31 @@ export default function ProductEditScreen(props) {
       //TODO: dispatch update product
       dispatch(updateProduct({_id: productId, name, price, image, category, brand, countInStock, description}))
    }
+
+   const [loadingUpload, setLoadingUpload] = useState(false);
+   const [errorUpload, setErrorUpload] = useState('');
+
+   const userSignin = useSelector((state) => state.userSignin);
+   const { userInfo } = userSignin;
+   const uploadFileHandler = async (e) => {
+      const file = e.target.files[0];
+      const bodyFormData = new FormData();
+      bodyFormData.append('image', file);
+      setLoadingUpload(true);
+      try {
+         const { data } = await Axios.post('/api/uploads', bodyFormData, {
+         headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${userInfo.token}`,
+         },
+         });
+         setImage(data);
+         setLoadingUpload(false);
+      } catch (error) {
+         setErrorUpload(error.message);
+         setLoadingUpload(false);
+      }
+   };
    
    return (
       <div>
@@ -94,6 +120,19 @@ export default function ProductEditScreen(props) {
                      ></input>
                   </div>
                   <div>
+                     <label htmlFor="imageFile">Image File</label>
+                     <input
+                        type="file"
+                        id="imageFile"
+                        label="Choose Image"
+                        onChange={uploadFileHandler}
+                     ></input>
+                     {loadingUpload && <LoadingBox></LoadingBox>}
+                     {errorUpload && (
+                        <MessageBox variant="danger">{errorUpload}</MessageBox>
+                     )}
+                     </div>
+                  <div>
                      <label htmlFor="category">Category</label>
                      <input
                         id="category"
@@ -112,6 +151,10 @@ export default function ProductEditScreen(props) {
                         value={brand}
                         onChange={(e) => setBrand(e.target.value)}
                      ></input>
+                     {loadingUpload && <LoadingBox></LoadingBox>}
+                     {errorUpload && (
+                        <MessageBox variant="danger">{errorUpload}</MessageBox>
+                     )}
                   </div>
                   <div>
                      <label htmlFor="countInStock">Count In Stock</label>
