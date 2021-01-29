41aad9df86262fccf7407871b00c8813683033a7
SuayMack <pry_chu@hotmail.com>
2021-01-29 00:40:10 -0300

    Delete Users

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 import {
+   USER_DELETE_FAIL,
+   USER_DELETE_REQUEST,
+   USER_DELETE_SUCCESS,
    USER_DETAILS_FAIL,
    USER_DETAILS_REQUEST,
    USER_DETAILS_SUCCESS,
@@ -121,3 +124,22 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
          dispatch({ type: USER_LIST_FAIL, payload: message });
       }
    };
+
+   export const deleteUser = (userId) => async (dispatch, getState) => {
+      dispatch({ type: USER_DELETE_REQUEST, payload: userId });
+      const {
+         userSignin: { userInfo },
+      } = getState();
+      try {
+         const { data } = await Axios.delete(`/api/users/${userId}`, {
+            headers: { Authorization: `Bearer ${userInfo.token}` },
+         });
+         dispatch({ type: USER_DELETE_SUCCESS, payload: data });
+      } catch (error) {
+         const message =
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message;
+         dispatch({ type: USER_DELETE_FAIL, payload: message });
+      }
+    };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducers } from './reducers/cartReducers';
 import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productUpdateReducer } from './reducers/productReducers';
-import { userDetailsReducer, userListReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
+import { userDeleteReducer, userDetailsReducer, userListReducer, userRegisterReducer, userSigninReducer, userUpdateProfileReducer } from './reducers/userReducers';
 import { orderCreateReducer, orderDetailsReducer, orderMineListReducer, orderPayReducer, orderListReducer, orderDeleteReducer, orderDeliverReducer } from './reducers/orderReducers';
 
 const initialState = {
@@ -40,6 +40,7 @@ const reducer = combineReducers({
     orderDelete: orderDeleteReducer,
     orderDeliver: orderDeliverReducer,
     userList: userListReducer,
+    userDelete: userDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
