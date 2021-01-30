e071980d13f0b48541f0ae0f648b473ec8bac714
SuayMack <pry_chu@hotmail.com>
2021-01-30 14:46:06 -0300

    update Edit User

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -100,8 +100,9 @@ userRouter.put('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, res) => 
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    user.isSeller = req.body.isSeller || user.isSeller;
-    user.isAdmin = req.body.isAdmin || user.isAdmin;
+    user.isSeller = Boolean(req.body.isSeller);
+    user.isAdmin = Boolean(req.body.isAdmin);
+    // user.isAdmin = req.body.isAdmin || user.isAdmin;
     const updatedUser = await user.save();
     res.send({ message: 'User Updated', user: updatedUser });
   } else {
